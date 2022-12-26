391a870f9ecb91c5255a1987c01736c2002223e4
Devnarayan Chouhan <devnarayanchouhan47@gmail.com>
2022-12-26 19:54:50 +0530

    update by dev 3

--- controllers/auth.js
+++ controllers/auth.js
@@ -37,10 +37,8 @@ export const login = async (req, res) => {
         }
 
         const token = jwt.sign ({ email: existinguser.email, id:existinguser._id }, process.env.JWT_SECRET , { expiresIn: '1h'});
-        // res.status(200).json({ result: existinguser, token })
-        return res.status(200).json({message:"Login Successful"});
-
-
+        res.status(200).json({ result: existinguser, token })
+        
     } catch(error)  {
         res.status(500).json("Something went wrong....")
     }
